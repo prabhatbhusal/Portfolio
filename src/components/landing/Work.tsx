c82aeb786f81e1620d4dfd0a208fdebed1b548f5
Prabhat Bhusal <prabhatbhusal777@gmail.com>
2026-06-11 16:20:26 +0545

    adding more features

--- src/components/landing/Work.tsx
+++ src/components/landing/Work.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import Headerbanner from "../props/Headerbanner";
-
+import { ProjectData } from "@/lib/constants/data";
 const Work =()=>{
     return(
     <>
         <hr className="w-full    border-bg " />
         <section className="container">
             <Headerbanner header='Projects'/>
-            <div>
-                
+            <div className="grid grid-cols-2 gap-10 items-center justify-center">
+               {ProjectData.map((item,idx)=>(
+                <div className="border rounded-md borderbg greybg px-10 py-5 " key={idx}>
+                    <span className="text-xs mono  greytext ">{item.stack}</span>
+                    <h2 className=" font-bold  amber">{item.title}</h2>
+                    <p className="greytext mono text-xs">{item.description}</p>
+                    <div className="py-5 flex gap-2">
+                        <span className="borderbg greytext px-5 py-1 mono text-sm rounded-sm">{item.skills1}</span>
+                        <span className="borderbg greytext px-5 py-1 mono text-sm rounded-sm">{item.skills2}</span>
+                    </div>
+                </div>
+               ))
+
+               } 
             </div>
         </section>
     </>
